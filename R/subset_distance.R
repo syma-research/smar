13d013ffa9f5d8a0c6eb0dbe7e9121cac0ed7a4a
Siyuan Ma <sim653@mail.harvard.edu>
2019-05-24 11:43:55 -0400

    intialization

---
+++ R/subset_distance.R
@@ -0,0 +1,21 @@
+#' Subsetting a distance object by indicators
+#'
+#' @param dist an n x n distance object
+#' @param ind length-n subsetting indicator. Must be either character (sample names) or
+#' logical (TRUE/FALSE indicator).
+#'
+#' @return the subsetted distance object
+subset_distance <- function(dist, ind) {
+  if(class(dist) != "dist")
+    stop("dist must be a distance class!")
+  if(!(class(ind) %in% c("character", "logical")))
+    stop("ind must be a name/logical vector!")
+
+  dist_matrix <- as.matrix(dist)
+  if(class(ind) == "logical" & nrow(dist_matrix) != length(ind))
+    stop("Dimensions of dist and ind must match!")
+  if(class(ind) == "character" & !all(ind %in% rownames(dist_matrix)))
+    stop("ind must be a subset of the row/col names of dist!")
+
+  return(as.dist(dist_matrix[ind, ind]))
+}
